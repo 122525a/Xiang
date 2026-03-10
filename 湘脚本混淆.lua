d3c0df22d15ed419ba78bf626b45671b702db08d
Yttrium Hub <1466281531@qq.com>
2026-03-10 18:33:09 +0800

    Update 湘脚本混淆.lua

--- 湘脚本混淆.lua
+++ 湘脚本混淆.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/122525a/Xiang/refs/heads/main/%E6%B9%98%E8%84%9A%E6%9C%AC.txt.YttriumHubObfuscator.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/122525a/Xiang/refs/heads/main/loder-obfuscated.lua"))()
